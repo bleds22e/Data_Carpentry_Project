d191282604dc53b916851035b9486c6c74225df2
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2015-11-20 12:54:47 -0500

    Use dplyr instead of intermediate variables to get counts of DM and DO

--- Krats.R
+++ Krats.R
@@ -9,7 +9,7 @@ library(ggplot2)
 surveys <- read.csv("data/surveys.csv", header = TRUE)
 
 # get counts of DM and DO by period
-periods <- select(surveys, period, species)
-DM_DO <- filter(surveys, species == 'DM' | species == 'DO', period > 0)
-krats_period <- group_by(DM_DO, period, species)
-count_by_period <- summarise(krats_period, count = n())
+DM_DO_count <- select(surveys, period, species) %>% 
+               filter(species == 'DM' | species == 'DO', period > 0) %>% 
+               group_by(period, species) %>% 
+               summarise(count = n())
