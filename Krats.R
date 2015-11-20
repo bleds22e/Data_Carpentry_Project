514b6dee4cd57c9a7d837406995613580c459465
Ellen Bledsoe <ellen.bledsoe@ufl.edu>
2015-11-20 12:51:27 -0500

    Load packages and file, get counts of DM and DO by period

--- Krats.R
+++ Krats.R
@@ -1,3 +1,15 @@
 # Creator: Ellen Bledsoe
 # This code is for exploring kangaroo rat capture data for the Portal project
 
+# load packages
+library(dplyr)
+library(ggplot2)
+
+# load data
+surveys <- read.csv("data/surveys.csv", header = TRUE)
+
+# get counts of DM and DO by period
+periods <- select(surveys, period, species)
+DM_DO <- filter(surveys, species == 'DM' | species == 'DO', period > 0)
+krats_period <- group_by(DM_DO, period, species)
+count_by_period <- summarise(krats_period, count = n())
